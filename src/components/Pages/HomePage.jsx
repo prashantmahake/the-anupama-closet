6b208e5086cb4f3045c2b5e95e95d4e90fa2a085
Prashant Mahake <prashant.mahake@nielsen.com>
2023-12-21 16:52:27 +0530

    Added home page layout

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import classes from './HomePage.module.css'
 
 function HomePage() {
     return (
-        <div>
-            Home Page
+        <div className={classes.homePageWrapper}>
+            <div className={classes.coverImage}>
+
+            </div>
+
+            <div className={classes.homePageProductsContainer}>
+                <div className={classes.homePageProductWrapper}>
+                    <div className={classes.homePageProductImageWrapper}>
+                        <div className={`${classes.homePageProduct} ${classes.handloomSaree}`} />
+                    </div>
+                    <div className={classes.homePageProductName}>
+                        <p >Handloom Sarees</p>
+                    </div>
+                </div>
+
+                <div className={classes.homePageProductWrapper}>
+                    <div className={classes.homePageProductImageWrapper}>
+                        <div className={`${classes.homePageProduct} ${classes.designerOutfit}`} />
+                    </div>
+                    <div className={classes.homePageProductName}>
+                        <p >Designer Outfits</p>
+                    </div>
+                </div>
+
+                <div className={classes.homePageProductWrapper}>
+                    <div className={classes.homePageProductImageWrapper}>
+                        <div className={`${classes.homePageProduct} ${classes.handMadeJwellery}`} />
+                    </div>
+                    <div className={classes.homePageProductName}>
+                        <p >Handmade Jwellery</p>
+                    </div>
+                </div>
+
+                <div className={classes.homePageProductWrapper}>
+                    <div className={classes.homePageProductImageWrapper}>
+                        <div className={`${classes.homePageProduct} ${classes.oxidizedJwellery}`} />
+                    </div>
+                    <div className={classes.homePageProductName}>
+                        <p >Oxidized Jwellery</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
